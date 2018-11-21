d087081c765e352d88697303bd9ef3b22f0425c2
futhead <futhead@163.com>
2018-11-21 22:35:39 +0800

    【A】数据库

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
+from app.models import User
+from flask_login import login_required, login_user, logout_user
 
 #导入表单处理方法
 from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'futhead'}
     posts = [
@@ -27,10 +30,20 @@ def login():
     form = LoginForm()
     # 验证表格中的数据格式是否正确
     if form.validate_on_submit():
-        # 闪现的信息会出现在页面，当然在页面上要设置
-        flash('用户登录的名户名是:{} , 是否记住我:{}'.format(form.username.data, form.remember_me.data))
-        # 重定向至首页
+        # 根据表格里的数据进行查询，如果查询到数据返回User对象，否则返回None
+        user = User.query.filter_by(username=form.username.data).first()
+        # 判断用户不存在或者密码不正确
+        if user is None or not user.check_password(form.password.data):
+            # 如果用户不存在或者密码不正确就会闪现这条信息
+            flash('无效的用户名或密码')
+            # 然后重定向到登录页面
+            return redirect(url_for('login'))
+        # 这是一个非常方便的方法，当用户名和密码都正确时来解决记住用户是否记住登录状态的问题
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-        # 首次登录/数据格式错误都会是在登录界面
     return render_template('login.html',title='登 录',form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 class Config(object):
     #设置密匙要没有规律，别被人轻易猜到哦
     SECRET_KEY = 'futhead123456'
+    # 格式为mysql+pymysql://数据库用户名:密码@数据库地址:端口号/数据库的名字?数据库格式
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://futhead:futhead@localhost:3306/flaskblog?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
