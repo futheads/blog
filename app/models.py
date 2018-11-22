3dae462aa4b472ebc57e6c7ba8147d88db81cb3b
futhead <futhead@163.com>
2018-11-22 23:53:41 +0800

    【A】个人资料编辑功能

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow())
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
